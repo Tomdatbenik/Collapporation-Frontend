d30e72b0e8020be07c088d49d3ed3f1947754da9
royappeldoorn <roy.appeldoorn@gmail.com>
2020-04-01 11:06:09 +0200

    Added navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,20 @@
 <template>
-  <v-app
-    style="background: linear-gradient(90deg, #36d1dc 0%, #3bc8dd 21.04%, #47aee0 56.02%, #5b86e5 99.44%)"
-  >
-    <v-app-bar app color="rgb(255, 0, 0, 0)" flat>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="@/assets/Logo-v1.png"
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
-      <div class="headline mb-0 ml-4 .d-sm-none" id="app-title">
-        <span class="font-weight-bold">Coll</span
-        ><span class="font-italic" id="app-subtitle">app</span
-        ><span class="font-weight-bold">oration</span>
-      </div>
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+  <v-app>
+    <Navbar />
     <v-content>
-      <HelloWorld />
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld
+    Navbar
   },
 
   data: () => ({
@@ -52,12 +23,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-#app-title {
-  letter-spacing: 0.1em !important;
-}
-
-#app-subtitle {
-  margin-right: 0.09em;
-}
-</style>
+<style lang="scss"></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app-bar app class="app-bar" flat>
+    <v-container class="pa-0">
+      <div class="d-flex flex-row">
+        <div
+          class="headline mb-0 ml-4 .d-sm-none white--text d-flex align-center"
+          id="app-title"
+        >
+          <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="@/assets/Logo-v1.png"
+            transition="scale-transition"
+            width="50"
+          />
+        </div>
+        <div class="white--text headline ml-3">
+          <span class="font-weight-bold">Coll</span>
+          <span class="font-italic" id="app-subtitle">app</span>
+          <span class="font-weight-bold">oration</span>
+        </div>
+        <div class="d-flex ml-auto align-center">
+          test
+        </div>
+      </div>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#app-title {
+  letter-spacing: 0.1em !important;
+}
+
+#app-subtitle {
+  margin-right: 0.09em;
+}
+
+.app-bar {
+  background: linear-gradient(
+    90deg,
+    #36d1dc 0%,
+    #3bc8dd 21.04%,
+    #47aee0 56.02%,
+    #5b86e5 99.44%
+  );
+}
+</style>
