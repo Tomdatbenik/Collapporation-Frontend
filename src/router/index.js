05bc844248d93ec48c531ad82492143ffe11c398
royappeldoorn <roy.appeldoorn@gmail.com>
2020-04-08 15:26:34 +0200

    Correct routing fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import LandingPage from "../views/LandingPage.vue";
-import SubNavbar from "../components/SubNavbar.vue";
+import LandingPage from "@/views/LandingPage.vue";
+import SubNavbar from "@/components/navbar/SubNavbar.vue";
+import Home from "@/views/Home.vue";
+import Profile from "@/views/Profile/Profile.vue";
 
 Vue.use(VueRouter);
 
 const routes = [{
     path: "/",
-    name: "LandingPage",
+    name: "Landing",
     component: LandingPage
   },
   {
     path: "/",
-    name: "SubNavBar",
+    name: "Sub",
     component: SubNavbar,
     children: [{
-      path: "home",
-      name: "Home"
-    }]
+        path: "home",
+        name: "Home",
+        component: Home
+      },
+      {
+        path: "/profile/:id",
+        name: "profile",
+        component: Profile
+      }
+    ]
   }
 ];
 
