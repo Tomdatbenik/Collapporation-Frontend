d3b869e8067678620f4006875c0529322a496537
Vincent <vincentmuijtjens@ziggo.nl>
2020-05-06 15:57:18 +0200

    Added FeedFilter and FeedItem to homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,16 @@
 <template>
   <v-container>
-    Home Page
+    <FeedFilter></FeedFilter>
+    <FeedItem/>
   </v-container>
 </template>
 
 <script>
-export default {};
+import FeedFilter from "../components/feed/FeedFilter";
+import FeedItem from "../components/feed/FeedItem";
+export default {
+  components: {FeedItem, FeedFilter}
+};
 </script>
 
 <style lang="scss" scoped></style>
