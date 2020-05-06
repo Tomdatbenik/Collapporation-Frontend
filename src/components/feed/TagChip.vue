a3ed9e7a346ca320213abfe36a0f940f34724a89
Vincent <vincentmuijtjens@ziggo.nl>
2020-05-06 15:58:29 +0200

    First design tag chips

---
+++ src/components/feed/TagChip.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-chip class="ml-1" color="#EBE9E9">
+        <div style="color: #696969">
+            {{ text }}
+        </div>
+    </v-chip>
+</template>
+
+<script>
+    export default {
+        name: "TagChip",
+        props: {
+            text: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
