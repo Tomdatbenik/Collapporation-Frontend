4010de9c68d45859edc2f3431688100bd78ca2a3
Vincent <vincentmuijtjens@ziggo.nl>
2020-05-06 15:57:38 +0200

    Added tabs for filter

---
+++ src/components/feed/FeedFilter.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-card tile>
+            <v-tabs color="#696969" centered >
+                <v-tab style="color: #696969"><v-icon class="pr-1">mdi-fire</v-icon>HOT</v-tab>
+                <v-tab style="color: #696969"><v-icon class="pr-1">mdi-trending-up</v-icon>TRENDING</v-tab>
+                <v-tab style="color: #696969"><v-icon class="pr-1">mdi-clock-outline</v-icon>FRESH</v-tab>
+            </v-tabs>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "FeedFilter"
+    }
+</script>
+
+<style scoped>
+
+</style>
