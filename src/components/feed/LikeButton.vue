2a032678c40ebb28fac99aac38d890b5589ef8d8
Vincent <vincentmuijtjens@ziggo.nl>
2020-05-06 15:58:19 +0200

    First design like button

---
+++ src/components/feed/LikeButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-chip  color="#EBE9E9" class="ml-1 like-button" v-if="liked">
+        <v-icon color="#696969" class="mr-2">mdi-thumb-up</v-icon> {{ count }}
+    </v-chip>
+    <v-chip  color="#EBE9E9" class="ml-1 like-button" v-else>
+        <v-icon color="#696969" class="mr-2">mdi-thumb-up-outline</v-icon> {{ count }}
+    </v-chip>
+</template>
+
+<script>
+    export default {
+        name: "LikeButton",
+        data() {
+            return {
+                liked: true,
+                count: 0
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .like-button {
+        color: #696969;
+    }
+</style>
