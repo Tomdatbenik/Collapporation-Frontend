1fd744af3d8bcca54531c20430f16c801d358a5d
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-06 09:33:48 +0200

    removed 'null' parameter to do a correct get request

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,21 +1,20 @@
-/*eslint quote-props: ["error", "as-needed"]*/
 import axios from 'axios'
 
 export default {
   state: {
-    User: null
+    user: null
   },
   mutations: {
     SET_USER_DATA(state, userData) {
-      state.user = userData
-      localStorage.setItem('user', JSON.stringify(userData))
+      state.user = userData.data
+      localStorage.setItem('user', JSON.stringify(userData.data))
       // axios.defaults.headers.common['collapporationToken'] =
     }
   },
   actions: {
     authenticate({ commit }, credential) {
       axios
-        .get('http://localhost:8102/token-service/token/new', null, {
+        .get('http://localhost:8102/token-service/token/new', {
           headers: { idToken: credential }
         })
         .then(data => {
