8b5bad812edab200e3d2ccd7fc8dc124ab208104
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-12 15:27:10 +0200

    Initialize firebase and fetch information on app load

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
   <v-app>
     <Navbar />
+
     <v-content class="bg-color">
       <router-view />
+      <LoadingOverlay />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Navbar from "@/components/navbar/Navbar.vue";
+import Navbar from '@/components/navbar/Navbar.vue'
+import LoadingOverlay from '@/components/shared/LoadingOverlay.vue'
+import { mapActions, mapMutations } from 'vuex'
+import firebase from 'firebase'
 
 export default {
-  name: "App",
+  name: 'App',
 
   components: {
-    Navbar
+    Navbar,
+    LoadingOverlay
   },
 
   data: () => ({
     //
-  })
-};
+  }),
+
+  created() {
+    firebase.auth().useDeviceLanguage()
+    this.fetchInformation()
+  },
+
+  methods: {
+    ...mapActions('user', ['authenticate']),
+    ...mapMutations('user', {
+      loading: 'SET_LOADING'
+    }),
+    fetchInformation() {
+      this.loading(true)
+      firebase
+        .auth()
+        .getRedirectResult()
+        .then(result => {
+          console.log(result)
+          firebase
+            .auth()
+            .currentUser.getIdToken(true)
+            .then(idToken => {
+              console.log(idToken)
+              this.authenticate(idToken)
+            })
+        })
+        .catch(error => {
+          var errorCode = error.code
+          var errorMessage = error.message
+          console.log(errorCode + ': ' + errorMessage)
+          var email = error.email
+          var credential = error.credential
+          console.log(email + ' ' + credential)
+          this.loading(false)
+        })
+    }
+  }
+}
 </script>
 
 <style lang="scss">
