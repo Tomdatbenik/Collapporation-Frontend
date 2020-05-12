601e862d0d7c10f71b8c236984c48ea37121d0ff
royappeldoorn <roy.appeldoorn@gmail.com>
2020-05-12 15:28:58 +0200

    added loading overlay for login/logout

---
+++ src/components/shared/LoadingOverlay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-center">
+    <v-overlay :value="loading" opacity="1" light color="#fff">
+      <v-progress-circular
+        indeterminate
+        size="64"
+        color="blue-400"
+      ></v-progress-circular>
+    </v-overlay>
+    {{ loading }}
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState('user', ['loading'])
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
