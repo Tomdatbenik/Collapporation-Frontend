d3f218bbd518afa7fbb75bfb37db2136c8bfdd01
Nicky Janse <nicky.janse.nj@gmail.com>
2020-05-12 15:48:25 +0200

    add project page custom stepper

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,49 +1,132 @@
 <template>
-  <div>
-    <v-stepper v-model="currentStep">
-      <v-row>
-        <v-col>
-          <template v-for="step in steps">
-            <v-stepper-content
-              :key="`${step.number}-content`"
-              :step="step.number"
+  <v-container>
+    <v-row>
+      <v-col cols="9">
+        <v-card tile height="70vh">
+          <v-card-title color="#696969">
+            {{ this.currentStep.title }}
+            <v-divider
+              class="ml-3"
+              style="border-width: 1px; border-color: #696969"
+            ></v-divider>
+          </v-card-title>
+          <v-row no-gutters>
+            <div
+              v-if="currentStep.number === 1"
+              style="width: 100%; heigth: 100%;"
             >
-              <v-card
-                class="mb-12"
-                color="grey lighten-1"
-                height="200px"
-              ></v-card>
-
-              <v-btn color="primary" @click="nextStep(step.number)">
-                Continue
-              </v-btn>
-
-              <v-btn text>Cancel</v-btn>
-            </v-stepper-content>
-          </template>
-        </v-col>
-        <v-col>
-          <template v-for="step in steps">
-            <v-stepper-step
-              :key="`${step.number}-step`"
-              :complete="currentStep > step.number"
-              :step="step.number"
-              :editable="true"
+              <v-text-field
+                label="Title"
+                filled
+                clearable
+                class="ml-10 mr-10"
+              ></v-text-field>
+            </div>
+            <div v-if="currentStep.number === 2">
+              <v-file-input></v-file-input>
+            </div>
+            <div
+              v-if="currentStep.number === 3"
+              style="width: 100%; heigth: 100%;"
             >
-              {{ step.title }}
-            </v-stepper-step>
-          </template>
-        </v-col>
-      </v-row>
-    </v-stepper>
-  </div>
+              <v-textarea
+                label="Small description"
+                filled
+                clearable
+                no-resize
+                rows="5"
+                class="ml-10 mr-10"
+              ></v-textarea>
+            </div>
+            <div v-if="currentStep.number === 4">
+              Tags stuf enzo
+            </div>
+            <div v-if="currentStep.number === 5">
+              Links stuf enzo
+            </div>
+            <div v-if="currentStep.number === 6">
+              Markdown description stuf enzo
+            </div>
+            <div v-if="currentStep.number === 7">
+              Collapporators stuf enzo
+            </div>
+            <div v-if="currentStep.number === 8">
+              Overview stuf enzo
+            </div></v-row
+          >
+          <v-row no-gutters class="justify-center">
+            <v-btn v-if="currentStep.number === 1" rounded width="20vw"
+              >CANCEL</v-btn
+            >
+            <v-btn
+              v-else
+              @click="currentStep = steps[currentStep.number - 2]"
+              rounded
+              width="20vw"
+              >PREVIOUS</v-btn
+            >
+            <v-btn
+              v-if="currentStep.number !== 8"
+              color="teal lighten-2"
+              rounded
+              width="20vw"
+              class="ml-3"
+              @click="currentStep = steps[currentStep.number]"
+              >NEXT</v-btn
+            >
+            <v-btn
+              v-else
+              color="teal lighten-2"
+              rounded
+              width="20vw"
+              class="ml-3"
+              @click="saveProject()"
+            >
+              FINISH
+            </v-btn>
+          </v-row>
+        </v-card>
+      </v-col>
+
+      <v-col cols="3">
+        <v-card tile>
+          <v-card-title color="#696969">
+            Create Concept
+            <v-divider
+              class="ml-3"
+              style="border-width: 1px; border-color: #696969"
+            ></v-divider>
+          </v-card-title>
+          <v-row
+            no-gutters
+            v-for="step in steps"
+            :key="step.number"
+            @click="currentStep = step"
+            class="ml-6 mb-3"
+          >
+            <div
+              :style="[
+                currentStep.number == step.number
+                  ? { color: 'black', 'font-weight': 'bold' }
+                  : { color: 'grey' },
+                step.number < currentStep.number
+                  ? { 'text-decoration': 'line-through' }
+                  : '',
+              ]"
+            >
+              {{ step.number }}. {{ step.title }}
+            </div>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      currentStep: 1,
       steps: [
         { number: 1, title: "Title" },
         { number: 2, title: "Image" },
@@ -54,6 +137,7 @@ export default {
         { number: 7, title: "Collapporators" },
         { number: 8, title: "Overview" },
       ],
+      currentStep: { number: 1, title: "Title" },
       project: {
         title: { type: String },
         image: { type: String },
@@ -66,13 +150,6 @@ export default {
     };
   },
   methods: {
-    nextStep(step) {
-      if (step === this.steps) {
-        this.saveProject();
-      } else {
-        this.currentStep = step + 1;
-      }
-    },
     saveProject() {
       console.log(this.project);
     },
