6b1097f3951199659c02bb86c4919aa6246044e7
Nicky Janse <nicky.janse.nj@gmail.com>
2020-05-19 15:31:53 +0200

    added togglable markdown (styling needs to be done)

--- src/components/add-project/AddMarkdownDescription.vue
+++ src/components/add-project/AddMarkdownDescription.vue
@@ -1,30 +1,54 @@
 <template>
   <div>
-    <p class="mx-10">
-      The markdown description is where you put an extensive description of what
-      your project is about. You can provide additional information on the idea
-      behind the concept, design choices, used techniques together with images.
-    </p>
-    <v-textarea
-      autofocus
-      v-model="projectDescription"
-      label="Description"
-      filled
-      clearable
-      no-resize
-      rows="10"
-      class="mx-10"
-      @change="update"
-    ></v-textarea>
+    <v-row>
+      <p class="mx-10">
+        The markdown description is where you put an extensive description of
+        what your project is about. You can provide additional information on
+        the idea behind the concept, design choices, used techniques together
+        with images.
+      </p>
+      <v-switch
+        class="editPreviewSwitch"
+        v-model="togglePreview"
+        label="Preview"
+      ></v-switch>
+    </v-row>
+    <v-row>
+      <v-textarea
+        v-if="!togglePreview"
+        autofocus
+        v-model="projectDescription"
+        label="Description"
+        filled
+        clearable
+        no-resize
+        rows="10"
+        class="mx-10"
+        @change="update"
+      ></v-textarea>
+      <markdown-it-vue
+        v-else
+        class="md-body"
+        :content="this.projectDescription"
+      />
+    </v-row>
   </div>
 </template>
 
 <script>
+import MarkdownItVue from "markdown-it-vue";
+import "markdown-it-vue/dist/markdown-it-vue.css";
+
 export default {
+  components: {
+    MarkdownItVue,
+  },
   props: ["description"],
   data() {
     return {
-      projectDescription: this.description,
+      togglePreview: false,
+      projectDescription:
+        "# allo dit is een coole markdown" || this.description,
     };
   },
   methods: {
@@ -35,4 +59,9 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.editPreviewSwitch::before {
+  content: "Edit";
+  color: black;
+}
+</style>
