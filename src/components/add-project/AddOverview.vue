635d50006dbd8b3d4c1c231ccef2788cf39b87f0
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2020-05-20 13:19:37 +0200

    moved overview to separate component

---
+++ src/components/add-project/AddOverview.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ this.overiewProject }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: Object,
+  },
+  data() {
+    return {
+      valid: false,
+      overiewProject: this.project || null,
+    };
+  },
+  methods: {
+    previous() {
+      this.$emit("previous");
+    },
+    finish() {
+      this.$emit("save");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
