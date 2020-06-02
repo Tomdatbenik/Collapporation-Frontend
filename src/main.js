0d624a215f24d321ac5ed0cfaff1a500abc6894a
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-02 13:55:28 +0200

    Single quotes

--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
-import * as firebase from "firebase";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import * as firebase from 'firebase'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0oaQ94WOdbYp323AlQJuJQjfXt47JVqw",
-  authDomain: "collapporation.firebaseapp.com",
-  databaseURL: "https://collapporation.firebaseio.com",
-  projectId: "collapporation",
-  storageBucket: "collapporation.appspot.com",
-  messagingSenderId: "756136153245",
-  appId: "1:756136153245:web:abd0375d9d943eb0058474",
-  measurementId: "G-N9TV5Y97ZJ"
-};
+  apiKey: 'AIzaSyB0oaQ94WOdbYp323AlQJuJQjfXt47JVqw',
+  authDomain: 'collapporation.firebaseapp.com',
+  databaseURL: 'https://collapporation.firebaseio.com',
+  projectId: 'collapporation',
+  storageBucket: 'collapporation.appspot.com',
+  messagingSenderId: '756136153245',
+  appId: '1:756136153245:web:abd0375d9d943eb0058474',
+  measurementId: 'G-N9TV5Y97ZJ'
+}
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 new Vue({
   router,
   store,
   vuetify,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
