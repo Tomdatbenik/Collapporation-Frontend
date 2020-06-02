7ecbdab25af8155effb9c140b4ff04e724ff412d
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2020-06-02 15:41:04 +0200

    save image as base 64

--- src/store/modules/project.js
+++ src/store/modules/project.js
@@ -2,70 +2,90 @@ export default {
   namespaced: true,
   state: {
     addProject: {
-      title: "",
-      image: null,
-      smallDescription: "",
+      title: '',
+      img: '',
+      smallDescription: '',
       tags: [],
       links: [],
-      description: "",
-      collapporators: [],
-    },
+      description: '',
+      collapporators: []
+    }
   },
   mutations: {
     SAVE_ADDPROJECT(state, addProject) {
-      state.addProject = addProject;
+      state.addProject = addProject
     },
     SAVE_PROJECT_TITLE(state, title) {
-      state.addProject.title = title;
+      state.addProject.title = title
     },
     SAVE_PROJECT_IMAGE(state, image) {
-      state.addProject.image = image;
+      state.addProject.img = image
+    },
+    SAVE_IMAGE(state, image) {
+      state.image = image
     },
     SAVE_PROJECT_SMALLDESCRIPTION(state, smallDescription) {
-      state.addProject.smallDescription = smallDescription;
+      state.addProject.smallDescription = smallDescription
     },
     SAVE_PROJECT_TAGS(state, tags) {
-      state.addProject.tags = tags;
+      state.addProject.tags = tags
     },
     SAVE_PROJECT_LINKS(state, links) {
-      state.addProject.links = links;
+      state.addProject.links = links
     },
     SAVE_PROJECT_DESCRIPTION(state, description) {
-      state.addProject.description = description;
+      state.addProject.description = description
     },
     SAVE_PROJECT_COLLAPPORATORS(state, collapporators) {
-      state.addProject.collapporators = collapporators;
-    },
+      state.addProject.collapporators = collapporators
+    }
   },
   getters: {
-    getAddProject: (state) => {
-      return state.addProject;
-    },
+    getAddProject: state => {
+      return state.addProject
+    }
   },
   actions: {
     saveAddProject({ commit }, addProject) {
-      commit("SAVE_ADDPROJECT", addProject);
+      commit('SAVE_ADDPROJECT', addProject)
     },
     saveProjectTitle({ commit }, title) {
-      commit("SAVE_PROJECT_TITLE", title);
+      commit('SAVE_PROJECT_TITLE', title)
     },
-    saveProjectImage({ commit }, image) {
-      commit("SAVE_PROJECT_IMAGE", image);
+    async saveProjectImage({ commit }, image) {
+      if (image) {
+        // const url = URL.createObjectURL(image)
+        // await fetch(url)
+        //   .then(function(response) {
+        //     return response.blob()
+        //   })
+        //   .then(async function(blob) {
+        //     blobImage = new Uint8Array(await new Response(blob).arrayBuffer())
+        //   })
+        const reader = new FileReader()
+        reader.readAsDataURL(image)
+        reader.onloadend = () => {
+          commit('SAVE_PROJECT_IMAGE', reader.result)
+        }
+        reader.onerror = error => {
+          console.log('Error: ', error)
+        }
+      }
     },
     saveProjectSmallDescription({ commit }, smallDescription) {
-      commit("SAVE_PROJECT_SMALLDESCRIPTION", smallDescription);
+      commit('SAVE_PROJECT_SMALLDESCRIPTION', smallDescription)
     },
     saveProjectTags({ commit }, tags) {
-      commit("SAVE_PROJECT_TAGS", tags);
+      commit('SAVE_PROJECT_TAGS', tags)
     },
     saveProjectLinks({ commit }, links) {
-      commit("SAVE_PROJECT_LINKS", links);
+      commit('SAVE_PROJECT_LINKS', links)
     },
     saveProjectDescription({ commit }, description) {
-      commit("SAVE_PROJECT_DESCRIPTION", description);
+      commit('SAVE_PROJECT_DESCRIPTION', description)
     },
     saveProjectCollapporators({ commit }, collapporators) {
-      commit("SAVE_PROJECT_COLLAPPORATORS", collapporators);
-    },
-  },
-};
+      commit('SAVE_PROJECT_COLLAPPORATORS', collapporators)
+    }
+  }
+}
