9a2e6736dcce8c87f15dec0f14d6595b8bfc9d3c
Vincent <vincentmuijtjens@ziggo.nl>
2020-06-02 15:08:20 +0200

    Added sendLike method

--- src/service/api.js
+++ src/service/api.js
@@ -20,5 +20,13 @@ export default {
 
   getProfile(userId) {
     return api.get('user-service/user/' + userId)
+  },
+
+  sendLike(authToken, id) {
+    console.log(authToken)
+    return api.post('like-service/like/like', {
+      headers: { Authorization: authToken },
+      body: { object_id: id }
+    })
   }
 }
