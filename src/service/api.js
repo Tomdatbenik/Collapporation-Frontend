6e741073eed1d124db9d724a4d994bf20c80f957
royappeldoorn <roy.appeldoorn@gmail.com>
2020-06-02 13:55:48 +0200

    Configured axios with interceptor to set custom header

--- src/service/api.js
+++ src/service/api.js
@@ -1,24 +1,62 @@
 import axios from 'axios'
+import store from '@/store/index.js'
 
-const api = axios.create({
+let isRefreshing = false
+let subscribers = []
+
+const service = axios.create({
   baseURL: 'http://localhost:8102/',
   withCredentials: false
 })
 
-export default {
-  getNewToken(idToken) {
-    return api.get('token-service/token/new', {
-      headers: { idToken: idToken }
-    })
+service.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('user-token')
+    if (token) {
+      config.headers.common['collapporationToken'] = token
+    }
+    return config
   },
-
-  getRefreshedToken(collapToken) {
-    return api.get('token-service/token/refresh', {
-      headers: { collaporationToken: collapToken }
-    })
+  response => {
+    return response
   },
+  error => {
+    const {
+      config,
+      response: { status }
+    } = error
+    const originalRequest = config
 
-  getProfile(userId) {
-    return api.get('user-service/user/' + userId)
+    if (status === 401) {
+      if (!isRefreshing) {
+        isRefreshing = true
+        store.dispatch('user/getRefreshToken').then(newToken => {
+          isRefreshing = false
+          onRefreshed(newToken)
+        })
+      }
+
+      const retryOriginalRequest = new Promise(resolve => {
+        subscribeTokenRefresh(token => {
+          // replace the expired token and retry
+          originalRequest.headers['collapporationToken'] = token
+          resolve(axios(originalRequest))
+        })
+      })
+
+      return retryOriginalRequest
+    }
+
+    return Promise.reject(error)
   }
+)
+
+function subscribeTokenRefresh(cb) {
+  subscribers.push(cb)
 }
+
+function onRefreshed(newToken) {
+  subscribers = subscribers.filter(callback => callback(newToken))
+}
+
+export default service
