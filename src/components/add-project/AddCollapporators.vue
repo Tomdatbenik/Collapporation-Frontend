58c663844bc2d3a956c360e46610b54b20fe3f72
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2020-06-09 14:40:44 +0200

    style for images (scalable)

--- src/components/add-project/AddCollapporators.vue
+++ src/components/add-project/AddCollapporators.vue
@@ -32,7 +32,7 @@
         >{{ collapporator.name }}
       </v-chip>
     </v-row>
-    <v-row class="mt-10" no-gutters justify="center">
+    <v-row class="mt-10 mb-5" no-gutters justify="center">
       <v-btn @click="previous" rounded width="15vw">PREVIOUS</v-btn>
       <v-btn
         color="teal lighten-2"
@@ -47,50 +47,50 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-import AddToProjectDialog from "@/components/add-project/AddToProjectDialog.vue";
+import { mapGetters, mapActions } from 'vuex'
+import AddToProjectDialog from '@/components/add-project/AddToProjectDialog.vue'
 
 export default {
   components: {
-    AddToProjectDialog,
+    AddToProjectDialog
   },
   data() {
     return {
       collapporator: null,
       collapporators: [],
-      showAddDialog: false,
-    };
+      showAddDialog: false
+    }
   },
   computed: {
-    ...mapGetters("project", { project: "getAddProject" }),
+    ...mapGetters('project', { project: 'getAddProject' })
   },
   created() {
-    this.collapporators = this.project.collapporator;
+    this.collapporators = this.project.collapporator
   },
   methods: {
-    ...mapActions("project", ["saveProjectCollapporators"]),
+    ...mapActions('project', ['saveProjectCollapporators']),
 
     addCollapporator() {
-      this.collapporators.push(this.collapporator);
-      this.collapporator = undefined;
-      this.saveProjectCollapporators(this.collapporators);
+      this.collapporators.push(this.collapporator)
+      this.collapporator = undefined
+      this.saveProjectCollapporators(this.collapporators)
     },
     removeCollapporator(collapporator) {
       for (let i = 0; i < this.collapporators.length; i++) {
         if (this.collapporators[i] === collapporator) {
-          this.collapporators.splice(i, 1);
+          this.collapporators.splice(i, 1)
         }
       }
-      this.saveProjectCollapporators(this.collapporators);
+      this.saveProjectCollapporators(this.collapporators)
     },
     previous() {
-      this.$emit("previous");
+      this.$emit('previous')
     },
     next() {
-      this.$emit("next");
-    },
-  },
-};
+      this.$emit('next')
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss"></style>
--- src/components/add-project/AddImage.vue
+++ src/components/add-project/AddImage.vue
@@ -1,33 +1,31 @@
 <template>
-  <div>
-    <v-row no-gutters
-      ><v-col cols="7"
-        ><p class="mx-10">
-          This image will be shown in the feed. Pick something attractive to
-          lure as many collapporators as possible.
-        </p>
-        <v-form ref="form" v-model="valid">
-          <v-file-input
-            label="Image"
-            autofocus
-            prepend-icon="mdi-camera"
-            accept="image/png, image/jpeg, image/bmp, image/jfif, image/jpg"
-            v-model="image"
-            class="mx-10"
-            :rules="[rules.required, rules.size]"
-            @change="update"
-          ></v-file-input></v-form
-      ></v-col>
-      <v-col cols="5">
-        <div
-          class="mr-10"
-          style="width: 15vw; height: 20vh; border: 1px solid grey;"
-        >
-          <img v-if="url" :src="url" style="width: 100%; heigth: 100%;" />
+  <v-container>
+    <v-row class="d-flex flex-wrap"
+      ><v-col class="d-flex">
+        <v-avatar min-height="100" min-width="100" size="200" tile>
+          <v-img contain v-if="project.img" :src="project.img"></v-img>
+        </v-avatar> </v-col
+      ><v-col class="d-flex">
+        <div>
+          <p>
+            This image will be shown in the feed. Pick something attractive to
+            lure as many collapporators as possible.
+          </p>
+          <v-form ref="form" v-model="valid">
+            <v-file-input
+              label="Image"
+              autofocus
+              prepend-icon="mdi-camera"
+              accept="image/png, image/jpeg, image/bmp, image/jfif, image/jpg"
+              v-model="image"
+              :rules="[rules.required, rules.size]"
+              @change="update"
+            ></v-file-input
+          ></v-form>
         </div>
       </v-col>
     </v-row>
-    <v-row class="mt-10" no-gutters justify="center">
+    <v-row class="mt-10 mb-5" no-gutters justify="center">
       <v-btn @click="previous" rounded width="15vw">PREVIOUS</v-btn>
       <v-btn
         :disabled="!valid"
@@ -39,7 +37,7 @@
         >NEXT</v-btn
       >
     </v-row>
-  </div>
+  </v-container>
 </template>
 
 <script>
